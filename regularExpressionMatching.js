d600a6151c6c45bcda0f7795e99e23b2871de9e9
Sumit Kumar <sk4390789@gmail.com>
2022-07-24 07:34:37 +0530

    10 regularExpressionMatching.js

--- regularExpressionMatching.js
+++ regularExpressionMatching.js
@@ -1,4 +1,4 @@
-Constraints:
+// Constraints:
 // 1 <= s.length <= 20
 // 1 <= p.length <= 30
 // s contains only lowercase English letters.
@@ -46,15 +46,68 @@ const cases = [
     {
         s: "abc", p: "...*", ans: true
     },
+    {
+        s: "b", p: "a*b", ans: true
+    },
+    {
+        s: "b", p: ".*b", ans: true
+    },
 ];
 const regularExpresionMatching = (s, p) => {
-    if (p.length < 0) {
+    let m = new Map();
+
+    function dfs(i, j) {
+        if (m.has("" + i + j)) {
+            return m.get("" + i + j);
+        }
+        if (i >= s.length && j >= p.length) {
+            return true;
+        } else if (j >= p.length) {
+            return false;
+        }
+        let match = (i < s.length) && (s[i] == p[j] || p[j] == ".");
+        if ((j + 1 < p.length) && (p[j + 1] == "*")) {
+            let x = dfs(i, j + 2) ||
+                (match && dfs(i + 1, j));
+            m.set("" + i + j, x);
+            return m.get("" + i + j);
+        }
+        if (match) {
+            let x = dfs(i + 1, j + 1);
+            m.set("" + i + j, x);
+            return m.get("" + i + j);
+        }
         return false;
     }
 
-
-
-
-
-    return regularExpresionMatching(s, p)
+    return dfs(0, 0);
 }
+// var isMatch = function (s, p) {
+//     // TOP DOWN Memoization
+//     // console.log(s.length, p.length);
+
+//     function dfs(i, j) {
+//         if (i >= s.length && j >= p.length) {
+//             return true;
+//         }
+//         if (j >= p.length) {
+//             return false;
+//         }
+//         let match = i < s.length && (s[i] == p[j] || p[j] == '.');
+//         if ((j + 1 < p.length) && (p[j + 1] == '*')) {
+//             return (dfs(i, j + 2) || (match && dfs(dfs(i + 1, j))));
+//         }
+//         if (match) {
+//             return dfs(i + 1, j + 1);
+//         }
+//         return false;
+//     }
+
+//     return dfs(0, 0);
+// };
+let a = regularExpresionMatching(cases[3].s, cases[3].p);
+console.log("ANS", a, cases[3].ans);
+// cases.forEach(element => {
+//     let a = regularExpresionMatching(element.s, element.p);
+//     console.log("ANS", a, element.ans);
+// });
