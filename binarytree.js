d0e15e8541ef1469d015d4424930dff7a1e374f1
Sumit Kumar <sk4390789@gmail.com>
2022-08-10 20:58:21 +0530

    102.BinaryTreeLevelOrderTraversal

--- binarytree.js
+++ binarytree.js
@@ -5,18 +5,18 @@ class Node {
         this.right = null;
     }
 }
-// let a = new Node('a');
-// let b = new Node('b');
-// let c = new Node('c');
-// let d = new Node('d');
-// let e = new Node('e');
-// let f = new Node('f');
+let a = new Node('a');
+let b = new Node('b');
+let c = new Node('c');
+let d = new Node('d');
+let e = new Node('e');
+let f = new Node('f');
 
-// a.left = b;
-// a.right = c;
-// b.left = d;
-// b.right = e;
-// c.right = f;
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
 
 //     a
 //    / \
@@ -74,6 +74,27 @@ class Node {
 //     }
 //     return values;
 // }
+const breadthFirstValues = (root) => {
+    if (!root) return [];
+
+    const queue = [root];
+    const nodes = [];
+
+    while (queue.length > 0) {
+        let l = queue.length;
+        let level = [];
+        for (let i = 0; i < l; i++) {
+            let current = queue.shift();
+            if (current) {
+                level.push(current.val);
+                if (current.left) queue.push(current.left);
+                if (current.right) queue.push(current.right);
+            }
+        }
+        nodes.push(level);
+    }
+    return nodes;
+}
 // XxXXXXXXXXXXXXXXXXX
 // const breadthFirstValues = (root) => {
 //     if (!root) {
@@ -87,7 +108,7 @@ class Node {
 
 // }
 
-// console.log(breadthFirstValues(a));
+console.log(breadthFirstValues(a));
 
 // Tree Sum
 // let a = new Node(3);
@@ -111,21 +132,21 @@ class Node {
 // };
 // console.log(treeSum(a));
 
-let a = new Node(5);
-let b = new Node(11);
-let c = new Node(3);
-let d = new Node(4);
-let e = new Node(15);
-let f = new Node(12);
+// let a = new Node(5);
+// let b = new Node(11);
+// let c = new Node(3);
+// let d = new Node(4);
+// let e = new Node(15);
+// let f = new Node(12);
 
-a.left = b;
-a.right = c;
-b.left = d;
-b.right = e;
-c.right = f;
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
 
-const minValTree = (node) => {
-    if (!node) return Infinity;
-    return Math.min(minValTree(node.left), minValTree(node.right), node.val);
-}
-console.log(minValTree(a));
+// const minValTree = (node) => {
+//     if (!node) return Infinity;
+//     return Math.min(minValTree(node.left), minValTree(node.right), node.val);
+// }
+// console.log(minValTree(a));
