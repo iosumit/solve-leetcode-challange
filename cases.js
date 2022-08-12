fd96b472843e1dcd97eba9705cfd905dbdac5dfa
Sumit Kumar <sk4390789@gmail.com>
2022-08-12 17:38:08 +0530

    Update cases.js

--- cases.js
+++ cases.js
@@ -1,7 +1,8 @@
 export default class Case {
-    constructor(nums, ans, target) {
+    constructor(nums, ans, target, target1) {
         this.nums = nums;
         this.ans = ans;
         this.target = target;
+        this.target1 = target1;
     }
 }
